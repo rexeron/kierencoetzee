d36e792e8f58deb9a001bc470fe8a92fb6614072
Kieren <kieren@carderbee.com>
2022-03-24 16:18:31 +0000

    add favicon, add home icon to navbar, update harcoded copy

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 # Create your views here.
 def posts_view(request):
-    posts = Post.objects.all().order_by('-created')[:20]
+    posts = Post.objects.filter(status='PUBLISHED').order_by('-created')[:20]
     context = {
         'posts': posts
     }
--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from blog.models import Post
 
 # Create your views here.
 def home_view(request):
-    recent_posts = Post.objects.all().order_by('-created')[:3]
+    recent_posts = Post.objects.filter(status='PUBLISHED').order_by('-created')[:3]
     context = {
         'recent_posts': recent_posts
     }
